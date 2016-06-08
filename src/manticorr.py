310e760e260c8dd0283d770c8332d93f938197e3
mclaughlin6464 <mclaughlin6464@gmail.com>
2016-06-08 14:25:24 -0700

    first commit.

---
+++ src/manticorr.py
@@ -0,0 +1,11 @@
+'''
+Main file for this module.
+Takes the gadget base as input and an output name.
+Calculates the matter correlation function in a gadget output.
+Downsamples the particles randomly by a certain fraction.
+'''
+
+import argparse
+#import Corrfunc
+from .readGadgetSnapshot import *
+
